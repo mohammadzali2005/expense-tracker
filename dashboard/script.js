8a27ac49708f4da72c32cc54ab0413b60863de8d
mzking <mzking1384@gmail.com>
2026-08-25 20:40:04 +0300

    Added balance texts to js files. Corrected categories.

--- dashboard/script.js
+++ dashboard/script.js
@@ -13,3 +13,11 @@ closeModalBtn.addEventListener('click', () => {
     modal.style.display = "none";
     container.style.display = "flex";
 })
+
+// Balance in sidebar & header
+export const sidebarBalance = document.querySelector('.sidebar-balance');
+export const headerBalance = document.querySelector('.header-balance');
+export let balance = 0;
+
+sidebarBalance.textContent = `$${balance}`;
+headerBalance.textContent = `$${balance}`;
