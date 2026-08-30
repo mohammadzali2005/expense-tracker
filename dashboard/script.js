149ff17685b636e0cdac458648314ab070112fea
mzking <mzking1384@gmail.com>
2026-08-30 22:47:35 +0300

    Created transactions array to store each transaction

--- dashboard/script.js
+++ dashboard/script.js
@@ -19,7 +19,31 @@ export const sidebarBalance = document.querySelector('.sidebar-balance');
 export const headerBalance = document.querySelector('.header-balance');
 export let balance = 0;
 
-export let transactions = [];
-
 sidebarBalance.textContent = `$${balance}`;
 headerBalance.textContent = `$${balance}`;
+
+// Add transaction btn in quick entry
+export const qeAddBtn = documen.querySelector('.qu-add-btn');
+qeAddBtn.addEventListener('click', () => {
+
+});
+
+// Transactions
+export let transactions = [
+    {
+        description: "Monthly Salary",
+        date: "Aug 5, 2026",
+        category: "Salary",
+        amount: `$${2500}`
+    }
+];
+
+export const transactionsTable = document.querySelector('.transactions-table');
+transactionsTable.innerHTML += `
+    <tr>
+        <td>${transactions[0].description}</td>
+        <td>${transactions[0].date}</td>
+        <td><p class="td-category">${transactions[0].category}</p></td>
+        <td><p class="td-amount">${transactions[0].amount}</p></td>
+    </tr>
+`;
